4cc9a56df41132610d364b834e72cb4926ab4a16
Gonzalo Ribera <gonzalo.ribera@gmail.com>
2020-07-29 14:42:04 -0300

    Se agregan nuevos métodos y propiedades a la clase TrackableObject

--- trackable_object.py
+++ trackable_object.py
@@ -1,10 +1,37 @@
 class TrackableObject:
-	def __init__(self, objectID, centroid):
-		# store the object ID, then initialize a list of centroids
-		# using the current centroid
+	color = None
+	forma = None
+	medida = None
+	poli = None
+	bbox = None
+
+	def __init__(self, objectID, centroid, poli, bbox):
 		self.objectID = objectID
 		self.centroids = [centroid]
-		self.color = centroid[0]
-		# initialize a boolean used to indicate if the object has
-		# already been counted or not
+		self.poli = poli
+		self.bbox = bbox
 		self.counted = False
+
+	def getCentroidX(self):
+		return self.centroids[0][0]
+
+	def getCentroidY(self):
+		return self.centroids[0][1]
+
+	def setCentroid(self, centroid):
+		self.centroids = [centroid]
+
+	def setPoligon(self, poli):
+		self.poli = poli
+
+	def setBoundingBox(self, bbox):
+		self.bbox = bbox
+
+	def setColor(self, color):
+		self.color = color
+
+	def setForma(self, forma):
+		self.forma = forma
+
+	def setMedida(self, medida):
+		self.medida = medida
